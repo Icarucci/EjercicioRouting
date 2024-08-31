c6a3d107b843220db32debf103844c1332b4105a
Icarucci <ignaciocarucci@hotmail.com>
2024-08-31 19:29:35 -0300

    4

--- src/api.js
+++ src/api.js
@@ -3,13 +3,15 @@ import axios from "axios";
 
 const searchImages = async ()=>{
 
-    const response = await axios.get("https://api.unsplash.com/search/photos",{
+    const response = await axios.get("https://api.unsplash.com/search/photos/",{
         headers:{
                 Authorization:"Cliente-ID DK1aduZaXuIqg5AAjJVNkHWV1eWdORnvhfo5MZMWLCc"
+                
         },        
                 params:{
                     query:"Cats"
                 }
+                
     });
 
     console.log(response);
